69e6a6893665d54f8e9843645106564972feb7bf
maurodepalma <maurodepalma94@gmail.com>
2022-05-17 12:46:23 -0300

    updated mockAPI.js

--- cypress/integration/intercept/mockApi.js
+++ cypress/integration/intercept/mockApi.js
@@ -1,13 +1,30 @@
+// App for this project is under /testing-lists folder. repo: https://github.com/maurodepalma/testing-lists
+// Mock API responses may be useful during developmenr and testing when live data is either unavailable or unreliable
+// While designing an API, you can use mock APIs to work concurrently on the front and back-end, as well as to gather feedback from developers.
 describe('Intercept Demo', () => {
 
-    beforeEach(function(){
-        cy.visit('http://localhost:8888/')
+    it('Initial validation', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('#todo-list li')
+        .should('have.length', 2)
+        .and('contain', 'buy milk')
+        .and('contain', 'wash dishes')
     });
 
-    it('Initial validation', () => {
-        cy.get('.todo-list')
-        .should('have.lenght', 2)
+    it('Mocked API Response', ()=> {
+
+        cy.intercept('GET', '/todos', {
+            fixture: 'intercept/interceptFixture.json'
+        }).as('getTodos-Fixture'); //This is an alias
+
+        cy.visit('http://localhost:3000/')
+
+        cy.get('#todo-list li')
+        .should('have.length', 3)
         .and('contain', 'todo-a')
         .and('contain', 'todo-b')
+        .and('contain', 'todo-c')
     });
+
+
 });
